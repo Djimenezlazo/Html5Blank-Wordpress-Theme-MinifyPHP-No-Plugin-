0107e50aa67df7ae8a684c1b074039e2d2707541
Djimenezlazo <101285140+Djimenezlazo@users.noreply.github.com>
2022-03-09 15:42:25 -0300

    Se elimino page.php duplicado

--- page-home.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-<h2>test lalalal</h2>
-<?php echo get_template_directory() ?>
-<?php endwhile; ?>
-<?php endif; ?>
-<?php get_footer(); ?>
